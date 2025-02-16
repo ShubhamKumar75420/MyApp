4c6106a028891f10727b96d070bbfbaa0aa7ff99
Shubham_IITG <Shubhamk.0991@op.iitg.ac.in>
2025-02-16 18:45:19 +0530

    Reverting "Changing the App.java"
    This reverts commit 562acd71918531f14c08876b4fed95766270269c.
    
     # Your branch is up to date with 'origin/main'.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,6 +6,5 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Kya hall");
     }
 }
